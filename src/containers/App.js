70c570ae226f4d171f1157f5aa4f9ae13244538c
uzor1997 <66738486+uzor1997@users.noreply.github.com>
2020-12-29 00:13:21 +0100

    setting up my folder structure

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from'./App.module.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.Center}>
+        <h1>Let's start!!!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
